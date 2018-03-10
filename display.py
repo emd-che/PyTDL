dcb311915d9877a8a1097d600475171d713f78ac
baddin <baddin2016@gmail.com>
2018-03-10 13:16:43 -0800

    removed old commented code

--- display.py
+++ display.py
@@ -6,7 +6,6 @@ def displaying_list(list1):
 	return list_str 
 
 def display_main_menu():
-	#os.system('clear || cls')
 	main_menu_str = "-"*15 + "Hello in todo list. Wish for u a gd luck :^)" + "-"*15 +'''\n 
 	The main menu :  \n
 	\t1:Add item to the current list.\n
@@ -20,17 +19,11 @@ def display_main_menu():
 
 
 def display_current_list(current_list):
-	#os.system('cls||clear') 
 	current_list_str = 'The current list: \n\n'  + displaying_list(current_list.get_list()) + '\n\n'
-	#choice = input('c : Check\t q: Quit : ').lower()
 	return current_list_str
 
 
 def display_checked_list(checkedlist):
-	#os.system('cls||clear') 
 	checked_list_str = '''The checked list: ') \n
 	''' + displaying_list(checkedlist.get_archive()) + '\n\n'
-	#choice = input('Quit ? (y/n) : ')
-	#if choice == 'y':
-	#	display_main_menu()
 	return checked_list_str
